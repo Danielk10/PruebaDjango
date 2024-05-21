1e1aee1482ef6b9ef71643541106302d79c93ece
Danielk10 <danielpdiamon@gmail.com>
2024-05-20 20:08:24 -0400

    Final

--- PruebaDjango/settings.py
+++ PruebaDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inicio',
+    'incio',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'PruebaDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [PruebaDjango/plantillas],
+        'DIRS': ["PruebaDjango/plantillas",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
